2a42bfd2fb8e1a79ddf95cda353b13721f246908
CharlesBoadu <charles.boadu@stu.ucc.edu.gh>
2021-06-21 23:54:11 +0000

    First commit

---
+++ Assignment.py
@@ -0,0 +1,9 @@
+class Student:
+
+    def __init__(self, firstName, lastName, dateOfBirth, administrationNumber):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.dateOfBirth = dateOfBirth
+        self.administrationNumber = administrationNumber
+
+        
